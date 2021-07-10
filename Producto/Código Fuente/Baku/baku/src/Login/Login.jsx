352aa09712014a1623761cfda19626fd287f3f6f
francobosio <bosiofranco94@gmail.com>
2021-07-10 20:18:42 -0300

    login v3

--- Producto/Código Fuente/Baku/baku/src/Login/Login.jsx
+++ Producto/Código Fuente/Baku/baku/src/Login/Login.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
+
 import { makeStyles } from '@material-ui/core/styles';
 import { Search } from '@trejgun/material-ui-icons-google';
 import Container from '@material-ui/core/Container';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
-import FormControl from '@material-ui/core/FormControl'
-import FormHelperText from '@material-ui/core/FormHelperText'
-import FormLabel from '@material-ui/core/FormLabel'
-import Typography from '@material-ui/core/Typography'
-import Link from '@material-ui/core/Link'
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormLabel from '@material-ui/core/FormLabel';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel  from '@material-ui/core/FormControlLabel';
 
-
-const useStyles = makeStyles((theme) =>({
+const useStyles = makeStyles({
     fondo:{
         'background': 'linear-gradient(180deg, #076F55 0%, #FFFFFF 100%);',
     },
@@ -20,17 +22,25 @@ const useStyles = makeStyles((theme) =>({
     boton:{
         'font-weight': 'bold',
         'margin': '0 auto', 
-        'display': "flex",
+        'display': 'flex',
         'color':'#FFFFFF',
         'borderRadius': '5em',
     },
 
     botonGris:{
         'background-color': '#464D57',
+        '&:hover':{
+            background:'#1D2126',
+            color: '#FFFFFF',
+        }
     },
 
     botonVerde:{
         'background-color': '#4B9C8E',
+        '&:hover':{
+            background:'#076F55',
+            color: '#FFFFFF',
+        }
     },
 
     customContainer:{
@@ -44,16 +54,30 @@ const useStyles = makeStyles((theme) =>({
 
     customLabel:{
         'font-weight': 'bold',
-        'color': '#000000'
+        'color': '#000000',
     },
-}));
+
+    espaciado:{
+        'padding': '1em 0 1em 0',
+    },
+
+    customCheckbox:{
+        color: '#4B9C8E',
+    },
+
+    controlLabel:{
+        '& > .MuiCheckbox-colorSecondary.Mui-checked':{
+            color:'#4B9C8E' 
+        },
+    },
+});
 
 function Login(){
-    const classes = useStyles();
+    const estilos = useStyles();
     return(
-        <div className={classes.fondo}>
-            <Container className={classes.customContainer} maxWidth="xs" border={1}>
-                <Card className={classes.customCard}>
+        <div className={estilos.fondo}>
+            <Container className={estilos.customContainer} maxWidth="xs" border={1}>
+                <Card className={estilos.customCard}>
                     <CardMedia
                         component="img"
                         title="Logo de Baku"
@@ -61,31 +85,37 @@ function Login(){
                     />
                 </Card>
                 <hr/>
-                <Button variant="contained" className={classes.boton + ' ' + classes.botonGris} startIcon={<Search/>}>
+                <Button variant="contained" className={estilos.boton + ' ' + estilos.botonGris} startIcon={<Search/>}>
                     Continuar con Google
                 </Button>
                 <hr/>
                 <Container maxWidth="xs">
                     <FormControl margin="dense" fullWidth="true">
-                      <FormLabel className={classes.customLabel} htmlFor="email">Dirección de correo electrónico o nombre de usuario:</FormLabel>
-                      <input type="text" id="email" placeholder="Dirección de correo electrónico o nombre de usuario"></input>
+                        <FormLabel className={estilos.customLabel + ' ' + estilos.espaciado} htmlFor="email">Dirección de correo electrónico o nombre de usuario:</FormLabel>
+                        <input type="text" id="email" placeholder="Dirección de correo electrónico o nombre de usuario"></input>
+                        <FormLabel className={estilos.customLabel + ' ' + estilos.espaciado} htmlFor="password">Contraseña:</FormLabel>
+                        <input type="password" id="password" aria-describedby="helper-password" placeholder="Contraseña"></input>
+                        <FormHelperText className={estilos.espaciado} id="helper-password">
+                            Nunca compartas tu contraseña.
+                            <Typography>
+                                <Link href="https://www.google.com/">
+                                    ¿Olvidaste tu contraseña?
+                                </Link>
+                            </Typography>
+                        </FormHelperText>
+                        <FormControlLabel
+                            className={estilos.controlLabel} 
+                            margin="dense"
+                            control={<Checkbox className={estilos.customCheckbox}/>}
+                            label="Recordame"
+                        />
+                        <Button variant="contained" className={estilos.boton + ' ' + estilos.botonVerde}>
+                            Iniciar Sesión
+                        </Button>
                     </FormControl>
-                    <FormControl margin="dense" fullWidth="true">
-                      <FormLabel className={classes.customLabel} htmlFor="password">Contraseña:</FormLabel>
-                      <input type="password" id="password" aria-describedby="helper-password" placeholder="Contraseña"></input>
-                      <FormHelperText id="helper-password">Nunca compartas tu contraseña.</FormHelperText>
-                    </FormControl>
-                </Container>
-                <Typography>
-                    <Link href="https://www.google.com/">
-                        ¿Olvidaste tu contraseña?
-                    </Link>
-                </Typography>
-                <Button variant="contained" className={classes.boton + ' ' + classes.botonVerde}>
-                    Iniciar Sesion
-                </Button>
+                </Container>            
                 <hr/>
-                <Button variant="contained" className={classes.boton + ' ' + classes.botonVerde}>
+                <Button variant="contained" className={estilos.boton + ' ' + estilos.botonVerde}>
                     REGISTRATE EN BAKU
                 </Button>
             </Container>
