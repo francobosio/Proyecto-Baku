4d2bbcc98131f53687ee25ec210e1c2c8e03701f
francobosio <54374683+francobosio@users.noreply.github.com>
2021-09-16 20:54:14 -0300

    imagen en home

--- Producto/Código Fuente/Baku/baku/src/Home/Home.jsx
+++ Producto/Código Fuente/Baku/baku/src/Home/Home.jsx
@@ -11,9 +11,11 @@ import "@fontsource/roboto";
 import "typeface-kaushan-script";
 import Footy from '../Footy/Footy';
 import { LoginButton } from "../Login/LoginMetodo"
-
-//Imagenes
 import { Grid } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper'
+
+import imagenFondo from '../Imagenes/book_fantasy_5k.jpg';
+import logoBaku from '../Imagenes/Logo Baku Negro.png';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,7 +23,6 @@ const useStyles = makeStyles((theme) => ({
   },
   menuButton: {
     marginRight: theme.spacing(2),
-
   },
   title: {
     flexGrow: 1,
@@ -45,7 +46,7 @@ const useStyles = makeStyles((theme) => ({
     'font-weight': '400',
     'line-height': '1.2',
     'letter-spacing': '0.00938em',
-    color: '#FFFFFF',
+    color: '#00',
   },
   typographyKsText: {
     'font-size': '1rem',
@@ -63,15 +64,14 @@ const useStyles = makeStyles((theme) => ({
     'font-weight': '400',
     'line-height': '1.9',
     'letter-spacing': '0.00938em',
-    color: '#FFFFFF',
+    color: '#000',
   },
   grid: {
     display: "flex",
-    height: "93vh",
+    height: "100vh",
     "place-items": "center",
     "justify-content": "center",
     "flex-direction": "column",
-    'background': 'linear-gradient(180deg, #076F55 0%, #FFFFFF 110%);',
   },
   boton: {
     'font-weight': 'bold',
@@ -91,6 +91,14 @@ const useStyles = makeStyles((theme) => ({
       'color': '#FFFFFF',
     }
   },
+  divImagen:{
+    backgroundImage: `url(${imagenFondo})`,
+    'background-size': '100%'
+  },
+  paper:{
+    maxHeight:'30em',
+    maxWidth:'50em',
+  }
 }));
 
 export default function ButtonAppBar() {
@@ -98,25 +106,29 @@ export default function ButtonAppBar() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static" className={classes.color}>
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-          </IconButton>
-          <Typography variant="h1" className={classes.title}>
-          </Typography>
-          <Button className={classes.boton + ' ' + classes.botonVerde}>
-            <Link className={classes.link} to="/">Premium</Link>
-          </Button>
-          <Divider orientation="vertical" variant="middle" flexItem light />
-          <LoginButton text="Iniciar Sesión"></LoginButton>
-        </Toolbar>
-      </AppBar>
-      <Grid className={classes.grid}>
-        <Typography className={classes.typographyKsTitle}>Leer es Soñar</Typography>
-        <Typography className={classes.typographyKsText}>No dejes para mañana lo que puedes leer hoy.</Typography>
-        <Typography className={classes.typographyKsText}>Accede a cientos de libros originales en forma gratuita</Typography>
-        <LoginButton text="Ingresa a Baku"></LoginButton>
-      </Grid>
+      <div>
+        <AppBar position="static" className={classes.color}>
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            </IconButton>
+            <Typography variant="h1" className={classes.title}>
+            </Typography>
+            <Button className={classes.boton + ' ' + classes.botonVerde}>
+              <Link className={classes.link} to="/">Premium</Link>
+            </Button>
+            <Divider orientation="vertical" variant="middle" flexItem light />
+            <LoginButton text="Iniciar Sesión"></LoginButton>
+          </Toolbar>
+        </AppBar>
+        <div className={classes.divImagen}>
+          <Grid className={classes.grid}>
+            <img className={classes.paper} src={logoBaku}/>
+            <Typography className={classes.typographyKsText}>No dejes para mañana lo que puedes leer hoy.</Typography>
+            <Typography className={classes.typographyKsText}>Accedé a cientos de libros originales en forma gratuita</Typography>
+            <LoginButton text="Ingresa a Baku"></LoginButton>
+          </Grid>
+        </div>
+      </div>
       <Footy />
     </div>
   );
