622f8e353ba3f7d210498aa84ec3e11582ac6f66
Ariel <ariel.strasorier@gmail.com>
2021-11-06 15:54:45 -0300

    Imprementacion buscador + Refinamiento Registro v4.0

--- Producto/Código Fuente/Baku/Backend/src/routes/libros.routes.ts
+++ Producto/Código Fuente/Baku/Backend/src/routes/libros.routes.ts
@@ -16,7 +16,7 @@ router.get('/libros/delete/:id', libroCtrl.deleteLibro)
 router.put('/libros/:id', libroCtrl.updateLibro)
 
 router.get('/libros/buscar/:buscar', libroCtrl.buscarLibro)
-//serch by genero
+
 router.get('/libros/buscar/genero/:genero', libroCtrl.buscarLibroGenero)
 
 export default router
