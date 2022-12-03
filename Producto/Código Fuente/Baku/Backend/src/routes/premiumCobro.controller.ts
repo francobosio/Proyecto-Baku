1716879ea5c9a8c335e2fa79162c50eddd7c619c
francobosio <54374683+francobosio@users.noreply.github.com>
2022-12-03 20:12:39 -0300

    agrego acento a descripcion de premium y comienzo cobro premium

--- Producto/Código Fuente/Baku/Backend/src/routes/premiumCobro.controller.ts
+++ Producto/Código Fuente/Baku/Backend/src/routes/premiumCobro.controller.ts
@@ -1,8 +1,40 @@
 import { RequestHandler } from "express";
 import mongoose from 'mongoose'; 
+import PremiumCobro from "./PremiumCobro";
+import Usuario from "./Usuario";
 
 const ObjectId = mongoose.Types.ObjectId;
 
-export const createCobro: RequestHandler = async (req, res) => {
-   
+export const procesarCobroFront: RequestHandler = async (req, res) => {
+    const { front_Id, user_Id } = req.body;
+
+    let cobro = await PremiumCobro.findOne({webhookId: front_Id}).exec();
+
+    if (cobro) {
+        cobro.frontId = front_Id;
+        cobro.userId = user_Id;
+
+        if (cobro.estado == 'Aprobado'){
+            let fechaHoy = new Date(Date.now());
+            let fechaVencimiento = new Date(fechaHoy.setMonth(fechaHoy.getMonth()+1))
+            cobro.fechaVencimiento = fechaVencimiento;
+            
+            let usuario = await Usuario.findOne({auth0_id: user_Id});
+            if (usuario){
+                usuario.tipoUsuario = '2';
+                await usuario.save();
+            }
+        }
+    }
+    else {
+        cobro = new PremiumCobro({frontId: front_Id, userId: user_Id})
+    }
+
+    await cobro.save()
+    res.json(cobro)
 }
+
+export const procesarCobroWebhook: RequestHandler = async (req, res) => {
+
+}
+
