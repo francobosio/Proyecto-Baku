e557004d22035f8bcc3d03acb0e372cb9ea46c3e
Ariel <ariel.strasorier@gmail.com>
2022-12-05 19:15:54 -0300

    V2 05/12/2022 19:15

--- Producto/Código Fuente/Baku/baku/src/Drawer/Drawer.jsx
+++ Producto/Código Fuente/Baku/baku/src/Drawer/Drawer.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect} from 'react';
+import React, { useEffect } from 'react';
 import clsx from 'clsx';
-import { makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
@@ -57,10 +57,10 @@ const useStyles = makeStyles((theme) => ({
         overflowX: 'hidden',
         width: theme.spacing(7) + 1,
         [theme.breakpoints.only('md')]: {
-            width: theme.spacing(12) ,
-        },[theme.breakpoints.only('sm')]: {
+            width: theme.spacing(12),
+        }, [theme.breakpoints.only('sm')]: {
             width: theme.spacing(9),
-        },[theme.breakpoints.only('xs')]: {
+        }, [theme.breakpoints.only('xs')]: {
             width: '2.8em',
         },
     },
@@ -79,20 +79,20 @@ const useStyles = makeStyles((theme) => ({
     },
     ListItemIconProps: {
         [theme.breakpoints.only('md')]: {
-            marginLeft: '31%' ,
-        },[theme.breakpoints.only('sm')]: {
-            marginLeft: '23%' ,
-        },[theme.breakpoints.only('xs')]: {
-            marginLeft: '-30%' ,
+            marginLeft: '31%',
+        }, [theme.breakpoints.only('sm')]: {
+            marginLeft: '23%',
+        }, [theme.breakpoints.only('xs')]: {
+            marginLeft: '-30%',
         },
     },
     ListItemTextrops: {
         [theme.breakpoints.only('md')]: {
-            display: 'none' ,
-        },[theme.breakpoints.only('sm')]: {
-            display: 'none' ,
-        },[theme.breakpoints.only('xs')]: {
-            display: 'none' ,
+            display: 'none',
+        }, [theme.breakpoints.only('sm')]: {
+            display: 'none',
+        }, [theme.breakpoints.only('xs')]: {
+            display: 'none',
         }
     },
 }));
@@ -101,6 +101,7 @@ const useStyles = makeStyles((theme) => ({
 export const MiDrawer = (props) => {
     const location = useLocation();
     const [revisar, setrevisar] = React.useState(localStorage.getItem('tipoUsuario') === '3' ? true : false);
+    const [revisar2, setrevisar2] = React.useState(!revisar);
     const classes = useStyles();
     const [open, setOpen] = React.useState(localStorage.getItem('drawer_open') ? localStorage.getItem('drawer_open') : false);
     const [selectedIndex] = React.useState(props.pestaña);
@@ -108,6 +109,7 @@ export const MiDrawer = (props) => {
     const porcentajeMinAncho = "31%";
     const ocultarLogo = useMediaQuery(theme.breakpoints.down('md'));
     const matches = useMediaQuery(theme.breakpoints.down('lg'));
+   
     const handleDrawerOpen = () => {
         if (matches) {
             setOpen(false);
@@ -148,97 +150,115 @@ export const MiDrawer = (props) => {
                 }}
             >
                 <Box component="div" className={classes.toolbar}>
-                   {ocultarLogo ? null : <Image src={Logo} aspectRatio={2.4} color={"#4B9C8E"} /> } 
+                    {ocultarLogo ? null : <Image src={Logo} aspectRatio={2.4} color={"#4B9C8E"} />}
                 </Box>
                 <Divider />
                 <List>
-                    <Link onClick={handleLink} to="/Inicio" className={classes.link} >
-                        {/* Crear tooltip por debajo del cursor */}
-                        <Tooltip title="Inicio" enterDelay={1000} leaveDelay={200} enterNextDelay={1000} arrow>
-                            <ListItem button selected={selectedIndex === 1} >
-                                <ListItemIcon style={{minWidth: porcentajeMinAncho }} className={classes.ListItemIconProps} ><HomeOutlinedIcon style={{ color: "#FFFFFF" }} /></ListItemIcon>
-                                <ListItemText primary='Inicio' className={classes.ListItemTextrops} style={{ color: "#FFFFFF"  }} />
-                            </ListItem>
-                        </Tooltip>
-                    </Link>
-                    <Link onClick={handleLink} to="/Buscar" className={classes.link}>
-                        <Tooltip title="Buscar" enterDelay={1000} leaveDelay={200} enterNextDelay={1000} arrow>
-                            <ListItem button selected={selectedIndex === 2}>
-                                <ListItemIcon style={{minWidth: porcentajeMinAncho}} className={classes.ListItemIconProps}><SearchOutlinedIcon style={{ color: "#FFFFFF" }} /></ListItemIcon>
-                                <ListItemText primary="Buscar" className={classes.ListItemTextrops} style={{ color: "#FFFFFF" }} />
-                            </ListItem>
-                        </Tooltip> 
-                    </Link>
-                    <Link to="/Comunidad" className={classes.link} >
-                    <Tooltip title="Comunidad" enterDelay={1000} leaveDelay={200} enterNextDelay={1000} arrow>
-                        <ListItem button selected={selectedIndex === 3}>
-                        <ListItemIcon style={{minWidth: porcentajeMinAncho}} className={classes.ListItemIconProps}><AccountCircleIcon style={{ color: "#FFFFFF" }} /></ListItemIcon>
-                            <ListItemText primary='Comunidad' className={classes.ListItemTextrops} style={{ color: "#FFFFFF" }} />
-                        </ListItem>
-                        </Tooltip>
-                    </Link>
-                    <Link to="/Biblioteca" className={classes.link} >
-                        <Tooltip title="Biblioteca" enterDelay={1000} leaveDelay={200} enterNextDelay={1000} arrow>
-                            <ListItem button selected={selectedIndex === 4}>
-                                <ListItemIcon style={{minWidth: porcentajeMinAncho}} className={classes.ListItemIconProps}><MenuBookOutlinedIcon style={{ color: "#FFFFFF" }} /></ListItemIcon>
-                                <ListItemText primary='Biblioteca' className={classes.ListItemTextrops} style={{ color: "#FFFFFF"}} />
-                            </ListItem>
-                        </Tooltip>
-                    </Link>
-                    <Link to="/Suscripciones"className={classes.link} >
-                    <Tooltip title="Suscripciones" enterDelay={1000} leaveDelay={200} enterNextDelay={1000} arrow>
-                        <ListItem button selected={selectedIndex === 5}>
-                            <ListItemIcon style={{minWidth: porcentajeMinAncho}} className={classes.ListItemIconProps}><GroupAddIcon style={{ color: "#FFFFFF" }} /></ListItemIcon>
-                            <ListItemText primary='Suscripciones' className={classes.ListItemTextrops} style={{ color: "#FFFFFF" }} />
-                        </ListItem>
-                        </Tooltip>
-                    </Link>
-                    <Link to="/Publicar" className={classes.link} >
-                        <Tooltip title="Publicar" enterDelay={1000} leaveDelay={200} enterNextDelay={1000} arrow>
-                            <ListItem button selected={selectedIndex === 6}>
-                                <ListItemIcon style={{minWidth: porcentajeMinAncho}} className={classes.ListItemIconProps}><PublishOutlinedIcon style={{ color: "#FFFFFF" }} /></ListItemIcon>
-                                <ListItemText primary='Publicar' className={classes.ListItemTextrops} style={{ color: "#FFFFFF" }} />
-                            </ListItem>
-                        </Tooltip>
-                    </Link>
-                    {(revisar === true) ?
-                        <List>
-                            <Link to="/Revision" className={classes.link} >
-                                <Tooltip title="Revision"  enterDelay={1000} leaveDelay={200} enterNextDelay={1000} arrow>
+                    {revisar &&
+                        <Link onClick={handleLink} to="/Inicio" className={classes.link} >
+                            {/* Crear tooltip por debajo del cursor */}
+                            <Tooltip title="Inicio" enterDelay={1000} leaveDelay={200} enterNextDelay={1000} arrow>
+                                <ListItem button selected={selectedIndex === 1} >
+                                    <ListItemIcon style={{ minWidth: porcentajeMinAncho }} className={classes.ListItemIconProps} ><HomeOutlinedIcon style={{ color: "#FFFFFF" }} /></ListItemIcon>
+                                    <ListItemText primary='Inicio' className={classes.ListItemTextrops} style={{ color: "#FFFFFF" }} />
+                                </ListItem>
+                            </Tooltip>
+                        </Link>
+                    }
+                    {revisar2 &&
+                        <Link onClick={handleLink} to="/Buscar" className={classes.link}>
+                            <Tooltip title="Buscar" enterDelay={1000} leaveDelay={200} enterNextDelay={1000} arrow>
+                                <ListItem button selected={selectedIndex === 2}>
+                                    <ListItemIcon style={{ minWidth: porcentajeMinAncho }} className={classes.ListItemIconProps}><SearchOutlinedIcon style={{ color: "#FFFFFF" }} /></ListItemIcon>
+                                    <ListItemText primary="Buscar" className={classes.ListItemTextrops} style={{ color: "#FFFFFF" }} />
+                                </ListItem>
+                            </Tooltip>
+                        </Link>
+                    }
+                    {revisar &&
+                        <Link to="/Comunidad" className={classes.link} >
+                            <Tooltip title="Comunidad" enterDelay={1000} leaveDelay={200} enterNextDelay={1000} arrow>
+                                <ListItem button selected={selectedIndex === 3}>
+                                    <ListItemIcon style={{ minWidth: porcentajeMinAncho }} className={classes.ListItemIconProps}><AccountCircleIcon style={{ color: "#FFFFFF" }} /></ListItemIcon>
+                                    <ListItemText primary='Comunidad' className={classes.ListItemTextrops} style={{ color: "#FFFFFF" }} />
+                                </ListItem>
+                            </Tooltip>
+                        </Link>
+                    }
+                    {revisar2 &&
+                        <Link to="/Biblioteca" className={classes.link} >
+                            <Tooltip title="Biblioteca" enterDelay={1000} leaveDelay={200} enterNextDelay={1000} arrow>
+                                <ListItem button selected={selectedIndex === 4}>
+                                    <ListItemIcon style={{ minWidth: porcentajeMinAncho }} className={classes.ListItemIconProps}><MenuBookOutlinedIcon style={{ color: "#FFFFFF" }} /></ListItemIcon>
+                                    <ListItemText primary='Biblioteca' className={classes.ListItemTextrops} style={{ color: "#FFFFFF" }} />
+                                </ListItem>
+                            </Tooltip>
+                        </Link>
+                    }
+
+                    {revisar2 &&
+                        <Link to="/Suscripciones" className={classes.link} >
+                            <Tooltip title="Suscripciones" enterDelay={1000} leaveDelay={200} enterNextDelay={1000} arrow>
+                                <ListItem button selected={selectedIndex === 5}>
+                                    <ListItemIcon style={{ minWidth: porcentajeMinAncho }} className={classes.ListItemIconProps}><GroupAddIcon style={{ color: "#FFFFFF" }} /></ListItemIcon>
+                                    <ListItemText primary='Suscripciones' className={classes.ListItemTextrops} style={{ color: "#FFFFFF" }} />
+                                </ListItem>
+                            </Tooltip>
+                        </Link>
+                    }
+                    {revisar2 &&
+                        <Link to="/Publicar" className={classes.link} >
+                            <Tooltip title="Publicar" enterDelay={1000} leaveDelay={200} enterNextDelay={1000} arrow>
+                                <ListItem button selected={selectedIndex === 6}>
+                                    <ListItemIcon style={{ minWidth: porcentajeMinAncho }} className={classes.ListItemIconProps}><PublishOutlinedIcon style={{ color: "#FFFFFF" }} /></ListItemIcon>
+                                    <ListItemText primary='Publicar' className={classes.ListItemTextrops} style={{ color: "#FFFFFF" }} />
+                                </ListItem>
+                            </Tooltip>
+                        </Link>
+                    }
+                    {revisar &&
+                        <Link to="/Revision" className={classes.link} >
+                            <Tooltip title="Revision" enterDelay={1000} leaveDelay={200} enterNextDelay={1000} arrow>
                                 <ListItem button selected={selectedIndex === 7} >
-                                    <ListItemIcon style={{minWidth: porcentajeMinAncho}} className={classes.ListItemIconProps}><RateReviewOutlinedIcon style={{ color: "#FFFFFF" }} /></ListItemIcon>
+                                    <ListItemIcon style={{ minWidth: porcentajeMinAncho }} className={classes.ListItemIconProps}><RateReviewOutlinedIcon style={{ color: "#FFFFFF" }} /></ListItemIcon>
                                     <ListItemText primary='Revisión' className={classes.ListItemTextrops} style={{ color: "#FFFFFF" }} />
                                 </ListItem>
-                                </Tooltip>
-                            </Link>
-                            <Link to="/Reclamo" className={classes.link} >
-                                <Tooltip title="Reclamos"  enterDelay={1000} leaveDelay={200} enterNextDelay={1000} arrow>
+                            </Tooltip>
+                        </Link>
+                    }
+                    {revisar &&
+                        <Link to="/Reclamo" className={classes.link} >
+                            <Tooltip title="Reclamos" enterDelay={1000} leaveDelay={200} enterNextDelay={1000} arrow>
                                 <ListItem button selected={selectedIndex === 8} >
-                                    <ListItemIcon style={{minWidth: porcentajeMinAncho}} className={classes.ListItemIconProps}><ReportGmailerrorredOutlinedIcon style={{ color: "#FFFFFF" }} /></ListItemIcon>
+                                    <ListItemIcon style={{ minWidth: porcentajeMinAncho }} className={classes.ListItemIconProps}><ReportGmailerrorredOutlinedIcon style={{ color: "#FFFFFF" }} /></ListItemIcon>
                                     <ListItemText primary='Reclamos' className={classes.ListItemTextrops} style={{ color: "#FFFFFF" }} />
                                 </ListItem>
-                                </Tooltip>
-                            </Link>
-                            <Link to="/Estadistica" className={classes.link} >
-                                <Tooltip title="Reportes" enterDelay={1000} leaveDelay={200} enterNextDelay={1000}arrow>
+                            </Tooltip>
+                        </Link>
+                    }
+                    {revisar &&
+
+                        <Link to="/Estadistica" className={classes.link} >
+                            <Tooltip title="Reportes" enterDelay={1000} leaveDelay={200} enterNextDelay={1000} arrow>
                                 <ListItem button selected={selectedIndex === 9}>
-                                    <ListItemIcon style={{minWidth: porcentajeMinAncho}} className={classes.ListItemIconProps}><StackedBarChartIcon style={{ color: "#FFFFFF" }} /></ListItemIcon>
+                                    <ListItemIcon style={{ minWidth: porcentajeMinAncho }} className={classes.ListItemIconProps}><StackedBarChartIcon style={{ color: "#FFFFFF" }} /></ListItemIcon>
                                     <ListItemText primary='Reportes' className={classes.ListItemTextrops} style={{ color: "#FFFFFF" }} />
                                 </ListItem>
-                                </Tooltip>
-                            </Link>
-                            <Link to="/Ajustes" className={classes.link} >
-                                <ListItem button selected={selectedIndex === 10}>
-                                    <ListItemIcon style={{minWidth: porcentajeMinAncho}} className={classes.ListItemIconProps}><MiscellaneousServicesIcon style={{ color: "#FFFFFF" }} /></ListItemIcon>
-                                <Tooltip title="Ajustes"  enterDelay={1000} leaveDelay={200} enterNextDelay={1000}  arrow>
+                            </Tooltip>
+                        </Link>
+                    }
+                    {revisar &&
+                        <Link to="/Ajustes" className={classes.link} >
+                            <ListItem button selected={selectedIndex === 10}>
+                                <ListItemIcon style={{ minWidth: porcentajeMinAncho }} className={classes.ListItemIconProps}><MiscellaneousServicesIcon style={{ color: "#FFFFFF" }} /></ListItemIcon>
+                                <Tooltip title="Ajustes" enterDelay={1000} leaveDelay={200} enterNextDelay={1000} arrow>
                                     <ListItemText primary={`Ajustes`} className={classes.ListItemTextrops} style={{ color: "#FFFFFF" }} />
                                 </Tooltip>
-                                </ListItem>
-                            </Link>
-                        </List> :
-                        null}
+                            </ListItem>
+                        </Link>
+                    }
+                    <Divider />
                 </List>
-                <Divider />
             </Drawer>
             <Divider />
         </Box>
