2ea8d7081d5c61360cd70f675523fc4678a7ffad
francobosio <54374683+francobosio@users.noreply.github.com>
2022-12-12 14:36:55 -0300

    fix bug cobros

--- Producto/Código Fuente/Baku/Backend/src/routes/premiumCobro.controller.ts
+++ Producto/Código Fuente/Baku/Backend/src/routes/premiumCobro.controller.ts
@@ -22,143 +22,144 @@ export const procesarCobroFront: RequestHandler = async (req, res) => {
                 cobro.fechaVencimiento = fechaVencimiento;
             }
 
-            const plan = await PremiumPlan.findOne({ titulo: cobro.plan }).exec();
-            if (plan) {
+            const plan = await PremiumPlan.findOne({titulo: cobro.plan})
+            if (plan){
                 await Usuario.findOneAndUpdate({ _id: cobro.userId }, { tipoUsuario: "2", planPremium: "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=" + plan.urlCobro }, { new: true }).exec();
-            }
+            }      
         }
-        else {
-            cobro = await Cobro.findOne({ frontId: front_Id }).exec();
-            if (!cobro) {
-                cobro = new Cobro({ frontId: front_Id, userId: user_Id })
-            }
+    }
+    else {
+        cobro = await Cobro.findOne({ frontId: front_Id }).exec();
+        if (!cobro) {
+            cobro = new Cobro({ frontId: front_Id, userId: user_Id })
         }
-
-        await cobro.save()
-        res.json(cobro)
     }
 
-    export const procesarCobroWebhook: RequestHandler = async (req, res) => {
-        const { data, entity, action } = req.body;
-        if (entity === "preapproval") {
-            if (action === "created") {
-                // Se crea un nuevo cobro o se actualiza en caso de que ya este creado
-                let cobro = await Cobro.findOne({ frontId: data.id }).exec();
-                if (cobro) {
-                    cobro.webhookId = data.id;
-                    cobro.estado = "Creado";
-                } else {
-                    cobro = await Cobro.findOne({ webhookId: data.id }).exec();
-                    if (!cobro) {
-                        cobro = new Cobro({ webhookId: data.id, estado: "Creado" });
-                    }
+    await cobro.save()
+    res.json(cobro)
+}
+
+export const procesarCobroWebhook: RequestHandler = async (req, res) => {
+    const { data, entity, action } = req.body;
+    if (entity === "preapproval") {
+        if (action === "created") {
+            // Se crea un nuevo cobro o se actualiza en caso de que ya este creado
+            let cobro = await Cobro.findOne({ frontId: data.id }).exec();
+            if (cobro) {
+                cobro.webhookId = data.id;
+                cobro.estado = "Creado";
+            } else {
+                cobro = await Cobro.findOne({ webhookId: data.id }).exec();
+                if (!cobro) {
+                    cobro = new Cobro({ webhookId: data.id, estado: "Creado" });
                 }
-                await cobro.save();
-            } else if (action === "updated") {
-                let cobro = await Cobro.findOne({ webhookId: data.id }).exec();
+            }
+            await cobro.save();
+        } else if (action === "updated") {
+            let cobro = await Cobro.findOne({ webhookId: data.id }).exec();
 
-                const res = await fetch(`https://api.mercadopago.com/preapproval/${data.id}`, {
-                    headers: {
-                        'Authorization': `Bearer ${config.MP_ACCESS_TOKEN}`
-                    }
-                }).then(response => response.json())
-                    .then(async data => {
-                        if (cobro) {
-                            if (data.status === "authorized") {
-                                cobro.fechaVencimiento = data.next_payment_date;
-                                cobro.estado = "Aprobado"
-                                let plan = await PremiumPlan.findOne({ urlCobro: "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=" + data.preapproval_plan_id })
-                                if (plan) {
-                                    cobro.plan = plan.titulo;
-                                }
-
-                                if (cobro.userId) {
-                                    await Usuario.findOneAndUpdate({ _id: cobro.userId }, { tipoUsuario: "2", planPremium: "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=" + data.init_point }, { new: true }).exec();
-                                }
-                            } else {
-                                cobro.estado = "Cancelado";
-                                cobro.notificadoMes = false;
-                                cobro.fechaVencimiento = data.next_payment_date;
-                                if (cobro.fechaVencimiento.getMonth() == new Date(Date.now()).getMonth() + 1) {
-                                    const notification = new Notificacion({
-                                        'titulo': 'Vencimiento de suscripción',
-                                        'esNoleido': true,
-                                        'descripcion': 'El próximo mes vence su suscripción y perderá sus beneficios Premium',
-                                        'tipo': 'Premium'
-                                    })
-                                    cobro.notificadoMes = true;
-                                    notification.save()
-                                    await Usuario.findOneAndUpdate({ _id: cobro.userId }, { $push: { mensajes: notification } }).exec();
-                                }
+            const res = await fetch(`https://api.mercadopago.com/preapproval/${data.id}`, {
+                headers: {
+                    'Authorization': `Bearer ${config.MP_ACCESS_TOKEN}`
+                }
+            }).then(response => response.json())
+                .then(async data => {
+                    if (cobro) {
+                        if (data.status === "authorized") {
+                            cobro.fechaVencimiento = data.next_payment_date;
+                            cobro.estado = "Aprobado"
+                            let plan = await PremiumPlan.findOne({ urlCobro: "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=" + data.preapproval_plan_id })
+                            if (plan) {
+                                cobro.plan = plan.titulo;
                             }
-                            await cobro.save();
+
+                            if (cobro.userId) {
+                                await Usuario.findOneAndUpdate({ _id: cobro.userId }, { tipoUsuario: "2", planPremium: "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=" + data.init_point }, { new: true }).exec();
+                            }
+                        } else {
+                            cobro.estado = "Cancelado";
+                            cobro.notificadoMes = false;
+                            cobro.fechaVencimiento = data.next_payment_date;
+                            if (cobro.fechaVencimiento.getMonth() == new Date(Date.now()).getMonth() + 1) {
+                                const notification = new Notificacion({
+                                    'titulo': 'Vencimiento de suscripción',
+                                    'esNoleido': true,
+                                    'descripcion': 'El próximo mes vence su suscripción y perderá sus beneficios Premium',
+                                    'tipo': 'Premium'
+                                })
+                                cobro.notificadoMes = true;
+                                notification.save()
+                                await Usuario.findOneAndUpdate({ _id: cobro.userId }, { $push: { mensajes: notification } }).exec();
+                            }
                         }
-                    });
+                        await cobro.save();
+                    }
+                });
+        }
+    }
+    res.sendStatus(200);
+}
+
+export const obtenerCobroByUserId: RequestHandler = async (req, res) => {
+    const cobro = await Cobro.findOne({ userId: req.params.id });
+    if (!cobro) return res.status(204).json();
+
+    return res.json(cobro)
+}
+
+export const obtenerCobros: RequestHandler = async (req, res) => {
+    const cobros = await Cobro.aggregate([
+        { $addFields: { "userId": { $toObjectId: "$userId" } } },
+        {
+            $lookup: {
+                from: "usuarios",
+                localField: "userId",
+                foreignField: "_id",
+                as: "usuario"
             }
         }
-        res.sendStatus(200);
-    }
+    ]);
 
-    export const obtenerCobroByUserId: RequestHandler = async (req, res) => {
-        const cobro = await Cobro.findOne({ userId: req.params.id });
-        if (!cobro) return res.status(204).json();
+    return res.json(cobros)
+}
 
-        return res.json(cobro)
-    }
+export const actualizarEstadosUsuarios = async () => {
+    let fechaHoy = new Date(Date.now());
+    let cobros = await Cobro.find({ estado: 'Cancelado' });
 
-    export const obtenerCobros: RequestHandler = async (req, res) => {
-        const cobros = await Cobro.aggregate([
-            { $addFields: { "userId": { $toObjectId: "$userId" } } },
-            {
-                $lookup: {
-                    from: "usuarios",
-                    localField: "userId",
-                    foreignField: "_id",
-                    as: "usuario"
+    cobros.forEach(async (cobro) => {
+        if (cobro.fechaVencimiento < fechaHoy) {
+            if (cobro.userId) {
+                cobro.estado = 'Finalizado';
+                let usuario = await Usuario.findById(cobro.userId)
+                if (usuario) {
+                    usuario.tipoUsuario = '1';
+                    usuario.planPremium = '';
+                    usuario.save();
                 }
-            }
-        ]);
-
-        return res.json(cobros)
-    }
-
-    export const actualizarEstadosUsuarios = async () => {
-        let fechaHoy = new Date(Date.now());
-        let cobros = await Cobro.find({ estado: 'Cancelado' });
-
-        cobros.forEach(async (cobro) => {
-            if (cobro.fechaVencimiento < fechaHoy) {
-                if (cobro.userId) {
-                    cobro.estado = 'Finalizado';
-                    let usuario = await Usuario.findById(cobro.userId)
-                    if (usuario) {
-                        usuario.tipoUsuario = '1';
-                        usuario.planPremium = '';
-                        usuario.save();
-                    }
-                    cobro.save();
-                }
-            } else if (cobro.fechaVencimiento.getDay() === new Date(Date.now()).getDay() + 7) {
-                const notification = new Notificacion({
-                    'titulo': 'Vencimiento de suscripción',
-                    'esNoleido': true,
-                    'descripcion': 'En 7 días vence su suscripción y perderá sus beneficios Premium',
-                    'tipo': 'Premium'
-                })
-                notification.save()
-                await Usuario.findOneAndUpdate({ _id: cobro.userId }, { $push: { mensajes: notification } }).exec();
-                cobro.save();
-            } else if (cobro.fechaVencimiento.getMonth() === new Date(Date.now()).getMonth() + 1) {
-                const notification = new Notificacion({
-                    'titulo': 'Vencimiento de suscripción',
-                    'esNoleido': true,
-                    'descripcion': 'El próximo mes vence su suscripción y perderá sus beneficios Premium',
-                    'tipo': 'Premium'
-                })
-                cobro.notificadoMes = true;
-                notification.save()
-                await Usuario.findOneAndUpdate({ _id: cobro.userId }, { $push: { mensajes: notification } }).exec();
                 cobro.save();
             }
-        });
-    }
+        } else if (cobro.fechaVencimiento.getDay() === new Date(Date.now()).getDay() + 7) {
+            const notification = new Notificacion({
+                'titulo': 'Vencimiento de suscripción',
+                'esNoleido': true,
+                'descripcion': 'En 7 días vence su suscripción y perderá sus beneficios Premium',
+                'tipo': 'Premium'
+            })
+            notification.save()
+            await Usuario.findOneAndUpdate({ _id: cobro.userId }, { $push: { mensajes: notification } }).exec();
+            cobro.save();
+        } else if (cobro.fechaVencimiento.getMonth() === new Date(Date.now()).getMonth() + 1) {
+            const notification = new Notificacion({
+                'titulo': 'Vencimiento de suscripción',
+                'esNoleido': true,
+                'descripcion': 'El próximo mes vence su suscripción y perderá sus beneficios Premium',
+                'tipo': 'Premium'
+            })
+            cobro.notificadoMes = true;
+            notification.save()
+            await Usuario.findOneAndUpdate({ _id: cobro.userId }, { $push: { mensajes: notification } }).exec();
+            cobro.save();
+        }
+    });
+}
